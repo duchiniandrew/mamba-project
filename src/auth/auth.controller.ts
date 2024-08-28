85b45e891711fab955aad84a0bca378e49e61ded
Andrew Duchini <97451223+andrewoliveiraduchini@users.noreply.github.com>
2024-08-28 12:09:56 -0300

    set login endpoint as public

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, HttpCode, HttpStatus, Get, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from 'src/guard/auth.guard';
+import { Public } from 'src/decorators/isPublic.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -8,8 +9,9 @@ export class AuthController {
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
+    @Public()
     signIn(@Body() signInDto: Record<string, any>) {
-        return this.authService.signIn(signInDto.username, signInDto.password);
+        return this.authService.signIn(signInDto.email, signInDto.password);
     }
 
     @UseGuards(AuthGuard)
