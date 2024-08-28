0514883caa97a4e391b44f0c1cdc9dacae4639f4
Andrew Duchini <97451223+andrewoliveiraduchini@users.noreply.github.com>
2024-08-28 12:10:21 -0300

    added decrypt process to compare passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt'
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService, private jwtService: JwtService) { }
+    constructor(private userService: UserService, private jwtService: JwtService, private configService: ConfigService) { }
 
     async signIn(
         email: string,
         pass: string,
     ): Promise<{ access_token: string }> {
         const user = await this.userService.findOne({ email });
-        if (user?.password !== pass) {
+        const decryptPasswordMatch = await bcrypt.compare(pass, user.password);
+        if (!decryptPasswordMatch) {
             throw new UnauthorizedException();
         }
-        const payload = { sub: user.id, username: user.email };
+        const payload = { sub: user.id, email: user.email };
         return {
             access_token: await this.jwtService.signAsync(payload),
         };
