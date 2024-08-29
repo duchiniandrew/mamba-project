8e6b4eceee26f25b804a0b0a447ab29632b809a3
Andrew Duchini <97451223+andrewoliveiraduchini@users.noreply.github.com>
2024-08-29 17:48:15 -0300

    added types for role guard

--- src/guard/roles.guard.ts
+++ src/guard/roles.guard.ts
@@ -3,17 +3,27 @@ import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from 'src/decorators/roles.decorator';
 import { Role } from 'src/enum/role.enum';
 
+type Jwt = {
+    user: {
+        email: string
+        exp: number
+        iat: number
+        sub: number
+        roles?: Role[]
+    }
+}
+
 @Injectable()
 export class RolesGuard implements CanActivate {
     constructor(private reflector: Reflector) { }
 
     canActivate(context: ExecutionContext): boolean {
-        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+        const requiredRoles: Role[] = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
             context.getHandler(),
             context.getClass(),
         ]);
         if (!requiredRoles) return true;
-        const { user } = context.switchToHttp().getRequest();
-        return requiredRoles.some((role) => user.roles?.includes(role));
+        const { user } = context.switchToHttp().getRequest() as Jwt;
+        return requiredRoles.some((role: Role) => user.roles?.includes(role));
     }
 }
